38175b0a2139521863799efbf59c08341d1e936b
KanzakiKino <kino@knzk.work>
2018-06-03 10:52:33 +0900

    [add] Added rational utility module.

---
+++ src/math/rational.d
@@ -0,0 +1,14 @@
+// Written under LGPL-3.0 in the D programming language.
+// Copyright 2018 KanzakiKino
+module g4d.math.rational;
+import std.traits;
+
+N nextPower2 ( N ) ( N x )
+    if ( isNumeric!N )
+{
+    N y = 1;
+    while ( y < x ) {
+        y *= 2;
+    }
+    return y;
+}
