580e04c4a7d17306c2d54fad41b3d3c8e10d24b0
KanzakiKino <kino@knzk.work>
2018-06-03 21:10:46 +0900

    [add] Added dependency FreeType.

---
+++ src/ft/lib.d
@@ -0,0 +1,21 @@
+// Written under LGPL-3.0 in the D programming language.
+// Copyright 2018 KanzakiKino
+module g4d.ft.lib;
+import std.string;
+public import derelict.freetype.ft;
+
+// This is a template that enforces freetype functions.
+template enforce ( alias func )
+    if ( __traits(identifier,func).indexOf("FT_") == 0 )
+{
+    void enforce ( string file = __FILE__, size_t line = __LINE__, Args... ) ( Args args )
+    {
+        auto err = func( args );
+        static assert( is(typeof(err) == FT_Error),
+               "Failed enforcing the functions that doesn't return error code." );
+        if ( err != FT_Err_Ok ) {
+            enum funcName = __traits(identifier,func);
+            throw new FTException( funcName, err, file, line );
+        }
+    }
+}
