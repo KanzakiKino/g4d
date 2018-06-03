9f7168eba93c332ad6df4f4fe191f06db3ef3a44
KanzakiKino <kino@knzk.work>
2018-06-03 09:36:54 +0900

    [update] Changed Bitmap specs and Added type checker.

--- src/util/bitmap.d
+++ src/util/bitmap.d
@@ -2,16 +2,20 @@
 // Copyright 2018 KanzakiKino
 module g4d.util.bitmap;
 import g4d.math.vector;
-import std.conv;
+import std.algorithm,
+       std.conv;
 import core.stdc.string;
 
-class Bitmap ( uint BytePerPixel = 4 )
+class Bitmap ( Type = ubyte, size_t LengthPerPixel = 4 )
 {
-    protected ubyte[] _bits;
+    alias bitType        = Type;
+    enum  lengthPerPixel = LengthPerPixel;
+
+    protected Type[] _bits;
     @property bits () { return _bits; }
     @property ptr () { return _bits.ptr; }
 
-    protected uint _width, _rows;
+    protected size_t _width, _rows;
     @property width () { return _width; }
     @property rows () { return _rows; }
 
@@ -20,25 +24,40 @@ class Bitmap ( uint BytePerPixel = 4 )
         resize( sz );
         clear();
     }
-    this ( Size sz, ubyte* src )
+    this ( Size sz, Type* src, size_t srcByte )
     {
         this( sz );
-        memcpy( ptr, src, bits.length );
+        auto copyByte = min( srcByte, Type.sizeof*_bits.length );
+        memcpy( ptr, src, copyByte );
+    }
+    this ( Size sz, Type[] src )
+    {
+        this( sz );
+        _bits = src.dup;
     }
 
     protected void resize ( Size sz )
     {
         _width       = sz.x.to!uint;
         _rows        = sz.y.to!uint;
-        _bits.length = width*rows*BytePerPixel;
+        _bits.length = width*rows*LengthPerPixel;
     }
 
     void clear ()
     {
         memset( ptr, 0, bits.length );
     }
+
+    enum this_is_a_bitmap_class_of_g4d = true;
 }
 
-alias BitmapA    = Bitmap!1;
-alias BitmapRGB  = Bitmap!3;
-alias BitmapRGBA = Bitmap!4;
+alias BitmapA    = Bitmap!(ubyte,1);
+alias BitmapRGB  = Bitmap!(ubyte,3);
+alias BitmapRGBA = Bitmap!(ubyte,4);
+
+alias BitmapAf    = Bitmap!(float,1);
+alias BitmapRGBf  = Bitmap!(float,3);
+alias BitmapRGBAf = Bitmap!(float,4);
+
+enum isBitmap(T) =
+    __traits(hasMember,T,"this_is_a_bitmap_class_of_g4d");
