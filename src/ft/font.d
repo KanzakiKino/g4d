a51c060916f5a1d423b32a5dca0ce064b8f3999c
KanzakiKino <kino@knzk.work>
2018-06-04 19:45:39 +0900

    [add] Added FontFace.

--- src/ft/font.d
+++ src/ft/font.d
@@ -1,7 +1,10 @@
 // Written under LGPL-3.0 in the D programming language.
 // Copyright 2018 KanzakiKino
 module g4d.ft.font;
-import g4d.ft.lib;
+import g4d.ft.lib,
+       g4d.math.vector,
+       g4d.util.bitmap,
+       g4d.exception;
 import std.string;
 
 class Font
@@ -32,5 +35,30 @@ class Font
             enforce!FT_Done_FreeType( _library );
             _library = null;
         }
+    }
+
+    BitmapA render ( vec2i sz, dchar c )
+    {
+        throw new G4dException( "Not Implemented" );
+    }
+}
+
+class FontFace
+{
+    protected Font   _font;
+    @property ref font () { return _font; }
+
+    protected vec2i _size;
+    @property ref size () { return _size; }
+
+    this ( Font f, vec2i s )
+    {
+        _font = f;
+        _size = s;
+    }
+
+    BitmapA render ( dchar c )
+    {
+        return _font.render( _size, c );
     }
 }
