1ccdc6deda135d76d87d2d9edda400e923dc7093
KanzakiKino <kino@knzk.work>
2018-06-28 15:29:17 +0900

    [update] Updated an example program.

--- example/triangles/src/app.d
+++ example/triangles/src/app.d
@@ -11,25 +11,30 @@ class App
     protected RegularNgonElement!3 triangle;
     protected vec3[50] pos;
 
+    protected mat4 projection;
+    protected vec3 rotation;
+
     this ()
     {
         win    = new Window( vec2i(640,480), "TRIANGLES", WindowHint.Visible );
         shader = new Fill3DShader;
-        shader.projection = mat4.orthographic(
-                -320, 320, 240, -240, short.min, short.max );
 
         triangle = new RegularNgonElement!3;
-        triangle.resize(100f);
+        triangle.resize(50f);
 
         foreach ( ref v; pos ) {
-            v = vec3(uniform(-320,320),uniform(-240,240),uniform(short.min,short.max));
+            v = vec3(uniform(-320,320),uniform(-240,240),uniform(-1000,1000));
         }
+
+        projection = mat4.orthographic(
+                -320, 320, 240, -240, short.min, short.max );
+        rotation = vec3(0,0,0);
 
         win.handler.onMouseMove = delegate ( vec2 pt )
         {
-            const halfW = win.size.x/2, halfH = win.size.y/2;
-            shader.rotation.y = (pt.x/halfW)*PI;
-            shader.rotation.z = (pt.y/halfH)*PI;
+            const halfW = win.size.x, halfH = win.size.y;
+            rotation.y = (pt.x/halfW-0.5)*PI*2;
+            rotation.x = (pt.y/halfH-0.5)*PI*2;
         };
     }
 
@@ -41,7 +46,9 @@ class App
             win.resetFrame();
 
             shader.use();
-            shader.color = vec4(1f,1f,1f,0.1f);
+            shader.color = vec4(1f,1f,1f,0.3f);
+            shader.projection = projection *
+                mat4.rotation( rotation.x, rotation.y, rotation.z );
             foreach ( v; pos ) {
                 shader.translate = v;
                 triangle.draw( shader );
