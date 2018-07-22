c44eec3178bcee5a5cabb949871d95b4c77a3756
KanzakiKino <kino@knzk.work>
2018-07-22 12:02:36 +0900

    [fix] Fixed wrong mouse button enumeration.

--- src/g4d/glfw/type.d
+++ src/g4d/glfw/type.d
@@ -5,16 +5,13 @@ import g4d.glfw.lib;
 
 enum MouseButton
 {
-    _1     = GLFW_MOUSE_BUTTON_1,
-    _2     = GLFW_MOUSE_BUTTON_2,
-    _3     = GLFW_MOUSE_BUTTON_3,
+    Left   = GLFW_MOUSE_BUTTON_1,
+    Right  = GLFW_MOUSE_BUTTON_2,
+    Middle = GLFW_MOUSE_BUTTON_3,
     _4     = GLFW_MOUSE_BUTTON_4,
     _5     = GLFW_MOUSE_BUTTON_5,
     _6     = GLFW_MOUSE_BUTTON_6,
     _7     = GLFW_MOUSE_BUTTON_7,
-    Left   = GLFW_MOUSE_BUTTON_LEFT,
-    Right  = GLFW_MOUSE_BUTTON_RIGHT,
-    Middle = GLFW_MOUSE_BUTTON_MIDDLE,
 }
 
 enum Key
